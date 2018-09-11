d8bf1aaaa6c00842846681b6fd92e6c18ecad2dc
Chelsea Grimmett <c.grimmett@me.com>
2018-09-11 14:28:06 -0700

    six finished

--- app.js
+++ app.js
@@ -28,9 +28,20 @@ var fs = require('fs');
 
 // fs.readFile("./challenge4")
 // fs.writeFileSync('challenge4/copyfolder')
-// fs.createReadStream('challenge4/info.txt').pipe(fs.createWriteStream('challenge4/copyfolder'));
+// fs.createReadStream('challenge4/info.txt').pipe(fs.createWriteStream('challenge4/copyfolder/info.txt'));
 
-fs.readFileSync("challenge5/info.txt","utf8")
-var str = "My-Name-is-troy";
-str = str.replace(/-/g, ' ');
-console.log(str);
+// fs.readFileSync("challenge5/info.txt","utf8")
+// var str = "My-Name-is-troy";
+// str = str.replace(/-/g, ' ');
+// console.log(str);
+
+
+
+let numSix =fs.readdirSync("./challenge6");
+console.log(numSix);
+
+for(let i = 0; i < numSix.length; i++){
+  if(numSix[i].slice(numSix[i].length - 4) == ".txt"){
+  console.log(numSix[i])
+  }
+}
