2d1d4db171a88e61f0e1eb58e87774e159e57836
Chelsea Grimmett <c.grimmett@me.com>
2018-09-11 13:03:14 -0700

    first two challenges

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 var fs = require('fs');
+
+
+
+// fs.readFile("challenge1/info.txt","utf8",(err,data)=> {
+//   if(err){
+//     throw err;
+//   }
+//   console.log(data)
+// })
+
+
+
+ //  let text =fs.readFileSync("challenge2/info.txt" ,"utf8")
+ //
+ //  text = text +'Chelsea\n' ;
+ //  fs.writeFileSync("info.txt" , text)
+ //
+ // console.log(text)
+
+
+
+ fs.rename("challenge3/binfo.txt","utf8",(err,data) =>{
+  if(err){
+    throw err;
+  }
+  console.log(data)
+})
